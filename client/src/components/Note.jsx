91743874e6f1a943b7c8229ccba693b9900f274f
antoinech <smb.antoine@gmail.com>
2024-07-24 17:26:47 +0200

    feat(client/date): Date formatting

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -73,101 +73,107 @@ export default function Note(props) {
             );
     }
 
-        return (
-            <Card>
-                <div>
-                    {editState ?
-                        <Input placeholder="Titre" size="lg" color="primary" name='title' value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} />
-                        :
-                        <Typography level="title-lg" fontStyle={!title && "italic"}>{title ?? "Sans titre"}</Typography>
-                    }
-                    <Stack direction="row" spacing={3}>
-                        <Typography level="body-sm">{date}</Typography>
-                        {modified && <Typography level="body-sm" fontStyle={"italic"}>Modifié le {modified}</Typography>}
-                    </Stack>
-                    {!editState && !noActions && <IconButton
-                        aria-label="bookmark Bahamas Islands"
-                        variant="plain"
-                        color="neutral"
-                        sx={{ position: 'absolute', top: '0.875rem', right: '3rem' }}
-                        onClick={() => {
-                            setEditState(true)
-                            console.log(textareaRef.current.value)
-                            textareaRef.current.focus()
-                        }}
-                    >
-                        <EditIcon />
-                    </IconButton>}
-                    {!editState && !noActions && <IconButton
-                        aria-label="bookmark Bahamas Islands"
-                        variant="plain"
-                        color="danger"
-                        sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}
-                        onClick={() => setConfirmationOpen(true)}
-                    >
-                        <DeleteForever />
-                    </IconButton>}
-                </div>
-                <Textarea
-                    slotProps={{ textarea: { ref: textareaRef } }}
-                    value={editedText}
-                    onChange={(e) => setEditedText(e.target.value)}
-                    readOnly={!editState}
-                    endDecorator={
-                        <Box sx={{ display: 'flex', gap: 0.5, flex: 1 }}>
-                            {!noActions && <Button variant="outlined" color="neutral" onClick={() => {
-                                navigator.clipboard.writeText(text);
-                                enqueueSnackbar("Copié !", { variant: 'info', autoHideDuration: 2000 });
-                            }}>
-                                Copier
-                            </Button>}
-                            <Typography level="body-xs" alignContent={'end'} sx={{ ml: "auto" }}>
-                                {text.length} caractère(s)
-                            </Typography>
-                        </Box>
-                    }
-                />
-                <CardContent orientation="horizontal">
+    return (
+        <Card>
+            <div>
+                {editState ?
+                    <Input placeholder="Titre" size="lg" color="primary" name='title' value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} />
+                    :
+                    <Typography level="title-lg" fontStyle={!title && "italic"}>{title ?? "Sans titre"}</Typography>
+                }
+                <Stack direction="row" spacing={3}>
+                    <Typography level="body-sm">Crée le {new Intl.DateTimeFormat('fr-FR', {
+                        dateStyle: 'full',
+                        timeStyle: 'medium',
+                    }).format(new Date(date))}</Typography>
+                    {modified && <Typography level="body-sm" fontStyle={"italic"}>Modifié le {new Intl.DateTimeFormat('fr-FR', {
+                        dateStyle: 'full',
+                        timeStyle: 'medium',
+                    }).format(new Date(modified))}</Typography>}
+                </Stack>
+                {!editState && !noActions && <IconButton
+                    aria-label="bookmark Bahamas Islands"
+                    variant="plain"
+                    color="neutral"
+                    sx={{ position: 'absolute', top: '0.875rem', right: '3rem' }}
+                    onClick={() => {
+                        setEditState(true)
+                        console.log(textareaRef.current.value)
+                        textareaRef.current.focus()
+                    }}
+                >
+                    <EditIcon />
+                </IconButton>}
+                {!editState && !noActions && <IconButton
+                    aria-label="bookmark Bahamas Islands"
+                    variant="plain"
+                    color="danger"
+                    sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}
+                    onClick={() => setConfirmationOpen(true)}
+                >
+                    <DeleteForever />
+                </IconButton>}
+            </div>
+            <Textarea
+                slotProps={{ textarea: { ref: textareaRef } }}
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+                readOnly={!editState}
+                endDecorator={
+                    <Box sx={{ display: 'flex', gap: 0.5, flex: 1 }}>
+                        {!noActions && <Button variant="outlined" color="neutral" onClick={() => {
+                            navigator.clipboard.writeText(text);
+                            enqueueSnackbar("Copié !", { variant: 'info', autoHideDuration: 2000 });
+                        }}>
+                            Copier
+                        </Button>}
+                        <Typography level="body-xs" alignContent={'end'} sx={{ ml: "auto" }}>
+                            {text.length} caractère(s)
+                        </Typography>
+                    </Box>
+                }
+            />
+            <CardContent orientation="horizontal">
 
-                </CardContent>
-                {editState && <CardActions buttonFlex="0 1 120px">
-                    <Button variant="soft" color="primary" sx={{ mr: "auto" }} onClick={cancelEdit}>
+            </CardContent>
+            {editState && <CardActions buttonFlex="0 1 120px">
+                <Button variant="soft" color="primary" sx={{ mr: "auto" }} onClick={cancelEdit}>
+                    Annuler
+                </Button>
+                <Button variant="solid" color="primary" sx={{ ml: "auto" }} onClick={confirmEdit}>
+                    Modifier
+                </Button>
+            </CardActions>}
+            <ConfirmationPopup open={confirmationOpen} setOpen={setConfirmationOpen} confirmAction={deleteNote} content={<Note noActions={true} {...props} />} />
+        </Card>
+
+    )
+}
+
+function ConfirmationPopup({ open, setOpen, content, confirmAction }) {
+    return (
+        <Modal open={open} onClose={() => setOpen(false)}>
+            <ModalDialog variant="outlined" role="alertdialog">
+                <DialogTitle>
+                    <WarningRoundedIcon />
+                    Confirmation
+                </DialogTitle>
+                <Divider />
+                <DialogContent>
+                    Voulez-vous supprimer définitivement le contenu suivant:
+                    <div>
+                        {content}
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="solid" color="danger" onClick={() => { setOpen(false); confirmAction() }}>
+                        Supprimer
+                    </Button>
+                    <Button variant="plain" color="neutral" onClick={() => setOpen(false)}>
                         Annuler
                     </Button>
-                    <Button variant="solid" color="primary" sx={{ ml: "auto" }} onClick={confirmEdit}>
-                        Modifier
-                    </Button>
-                </CardActions>}
-                <ConfirmationPopup open={confirmationOpen} setOpen={setConfirmationOpen} confirmAction={deleteNote} content={<Note noActions={true} {...props} />} />
-            </Card>
-
-        )
-    }
-
-    function ConfirmationPopup({ open, setOpen, content, confirmAction }) {
-        return (
-            <Modal open={open} onClose={() => setOpen(false)}>
-                <ModalDialog variant="outlined" role="alertdialog">
-                    <DialogTitle>
-                        <WarningRoundedIcon />
-                        Confirmation
-                    </DialogTitle>
-                    <Divider />
-                    <DialogContent>
-                        Voulez-vous supprimer définitivement le contenu suivant:
-                        <div>
-                            {content}
-                        </div>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button variant="solid" color="danger" onClick={() => { setOpen(false); confirmAction() }}>
-                            Supprimer
-                        </Button>
-                        <Button variant="plain" color="neutral" onClick={() => setOpen(false)}>
-                            Annuler
-                        </Button>
-                    </DialogActions>
-                </ModalDialog>
-            </Modal>
-        )
-    }
+                </DialogActions>
+            </ModalDialog>
+        </Modal>
+    )
+}
